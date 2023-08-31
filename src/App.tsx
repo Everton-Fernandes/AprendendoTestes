c64a61eb9baa961034755400ac999aba75211857
Everton-Fernandes <e.everton.elf@gmail.com>
2023-08-31 20:53:31 -0300

    Aprendendo testes com tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [form, setForm] = useState({
+    email: {
+      hasChanged: false,
+      value: ""
+    },
+    password: {
+      hasChanged: false,
+      value: ""
+    }
+  })
+
+  const isEmailValid = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='centralize'>
+      <form>
+        <input type="email" placeholder='Email' value={form.email.value}
+          onChange={event => setForm({
+            ...form, email: {
+              hasChanged: true, value: event.target.value
+            }
+          })}
+          data-testid='email' />
+        {
+          form.email.hasChanged && !form.email.value
+          && <div data-testid="email-required" className='textWhite'>Email é obrigatório !</div>
+        }
+        {
+          form.email.hasChanged && !isEmailValid(form.email.value)
+          && <div data-testid="email-invalid" className='textWhite'>Email inválido !</div>
+        }
+
+        <input type="password" placeholder='Senha' value={form.password.value}
+          onChange={event => setForm({
+            ...form, password: {
+              hasChanged: true, value: event.target.value
+            }
+          })}
+          data-testid="password" />
+        {
+          form.password.hasChanged && !form.password.value
+          && <div data-testid="password-required" className='textWhite'>Senha é obrigatória !</div>
+        }
+        <button type="button" className='clear'
+          data-testid="recover-password-button"
+          disabled={!isEmailValid(form.email.value)}>
+          Recuperar senha
+        </button>
+        <button type="button" className='solid'
+          data-testid="login-button"
+          disabled={!isEmailValid(form.email.value) || !form.password.value}>
+          Entrar
+        </button>
+        <button type="button" className='outline'>
+          Registrar
+        </button>
+      </form>
+    </main>
   );
 }
 
